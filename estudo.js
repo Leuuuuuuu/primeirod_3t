5c6fab891af292d7cd72e60c76ceaac70bae1ff1
Leuuuuuu <107219037+Leuuuuuuu@users.noreply.github.com>
2022-09-30 11:58:17 -0300

    Aula 30/09

---
+++ estudo.js
@@ -0,0 +1,15 @@
+let tabuada = 2;
+
+
+function escreva(){
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<BR>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<BR>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<BR>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<BR>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<BR>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<BR>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<BR>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<BR>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<BR>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<BR>");
+}
