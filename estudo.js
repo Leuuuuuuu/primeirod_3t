081706c7617c75007ac7d269ff33e78279f7df8a
Leuuuuuu <107219037+Leuuuuuuu@users.noreply.github.com>
2022-10-28 11:55:51 -0300

    Aula 28/10

--- estudo.js
+++ estudo.js
@@ -45,5 +45,10 @@ function moeda(atual){
     
   }
     
-  
+  function adicao() {
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) + Number(val2);
+    document.getElementById("Resultado").innetHTML = r;
+  }
   
